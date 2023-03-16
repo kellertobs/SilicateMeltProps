389fc48f8d930fc08c4beb6c3e5aa9ad55912198
Tobias Keller <kellertobs@gmail.com>
2023-03-16 14:20:04 +0100

    update run tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,7 @@
 using SilicateMeltProps
 using Test
 
-
-println("Testing package SilicateMeltProps.")
-
-println("Testing function DensityX.")
+@testset "DensityX" begin
 
 # calculate melt density of anhydrous N-MORB at 1200 C and 1.5 kbar
 #         SiO2   TiO2  Al2Oe  FeO(t) MgO  CaO    Na2O  K2O    H2O
@@ -12,21 +9,12 @@ oxd_wt = [50.42, 1.53, 15.13, 9.81, 7.76, 11.35, 2.83, 0.140, 0.0]  # [wt%]
 T = 1200.0  # [deg C]
 P = 1.5     # [kbar]
 
-println("Calculate density of anhydrous N-Morb at 1200 C, 1.5 kbar.")
 rho = DensityX(oxd_wt,T,P)  # [kg/m3]
 
 # compare to expected value (calculated with DensityX.xls)
-println("Calculated density is: ",rho)
 rho_exp = 2718.187297500990
-println("Expected density is: ",rho_exp)
-err = rho - rho_exp
+@test rho ≈ rho_exp
 
-# report test result
-if err<1e-9
-    println("Calculated density matches expected value, test passed.")
-else
-    error("Calculated density does not match expected value, test failed.")
-end
 
 # calculate melt density of hydrous N-MORB (1 wt% H2O) at 1200 C and 1.5 kbar
 #         SiO2   TiO2  Al2Oe  FeO(t) MgO  CaO    Na2O  K2O    H2O
@@ -34,59 +22,37 @@ oxd_wt = [50.42, 1.53, 15.13, 9.81, 7.76, 11.35, 2.83, 0.140, 1.0]  # [wt%]
 T = 1200.0  # [deg C]
 P = 1.5     # [kbar]
 
-println("Calculate density of anhydrous N-Morb at 1200 C, 1.5 kbar.")
 rho = DensityX(oxd_wt,T,P)  # [kg/m3]
 
 # compare to expected value (calculated with DensityX.xls)
-println("Calculated density is: ",rho)
 rho_exp = 2647.515936086210
-println("Expected density is: ",rho_exp)
-err = rho - rho_exp
+@test rho ≈ rho_exp #atol=1.0e-12
 
-# report test result
-if err<1e-9
-    println("Calculated density matches expected value, test passed.")
-else
-    error("Calculated density does not match expected value, test failed.")
 end
 
-
-println("Testing function Giordano08.")
+@testset "Giordano08" begin
 
 # calculate melt viscosity of anhydrous N-MORB at 1200 C and 1.5 kbar
 #         SiO2   TiO2  Al2Oe  FeO(t) MgO  CaO    Na2O  K2O    H2O
 oxd_wt = [50.42, 1.53, 15.13, 9.81, 7.76, 11.35, 2.83, 0.140, 0.0]  # [wt%]
 T = 1200.0  # [deg C]
 
-println("Calculate viscosity of anhydrous N-Morb at 1200 C, 1.5 kbar.")
 eta = Giordano08(oxd_wt,T)  # [Pas]
 
 # compare to expected value (calculated with grdViscosity.xls)
 eta_exp = 10^1.757695047576570
-err = eta - eta_exp
+@test eta ≈ eta_exp
 
-# report test result
-if err<1e-12
-    println("Calculated viscosity matches expected value, test passed.")
-else
-    error("Calculated viscosity does not match expected value, test failed.")
-end
 
 # calculate melt viscosity of hydrous N-MORB (1 wt% H2O) at 1200 C and 1.5 kbar
 #         SiO2   TiO2  Al2Oe  FeO(t) MgO  CaO    Na2O  K2O    H2O
 oxd_wt = [50.42, 1.53, 15.13, 9.81, 7.76, 11.35, 2.83, 0.140, 1.0]  # [wt%]
 T = 1200.0  # [deg C]
 
-println("Calculate viscosity of anhydrous N-Morb at 1200 C, 1.5 kbar.")
 eta = Giordano08(oxd_wt,T)  # [Pas]
 
 # compare to expected value (calculated with grdViscosity.xls)
 eta_exp = 10^1.05530134571660
-err = eta - eta_exp
+@test eta ≈ eta_exp atol=1.0e-12
 
-# report test result
-if err<1e-12
-    println("Calculated viscosity matches expected value, test passed.")
-else
-    error("Calculated viscosity does not match expected value, test failed.")
 end
